b5f813b0eca0d0e695ff5c63b1810e38681d4a2b
Malahmen <vegan.wolf.but.not.that.much@gmail.com>
2024-08-05 17:37:28 +0100

    fix: refactor some code.

--- cgs.sh
+++ cgs.sh
@@ -1,10 +1,6 @@
 #!/bin/sh
 
-# show warning about configurations:
-# subject.sh must be filled
-# subject-ca.sh must be filled for self-signed certificates
-# v3.ext file must be filled correctly
-
+# checks for permissions before executing.
 if [ "$(id -u)" -ne 0 ]; then
     echo "Must be run as root. Trying with sudo..."
     exec sudo HOME="$HOME" "$0" "$@"
@@ -28,21 +24,21 @@ $SCRIPT_DIR="$(cd "$(dirname "$0")" && pwd)"
 # Read parameters from command line
 read_parameters "$@"
 
-# if we want to generate .pem and .cert from a .crt file, we ignore everything else.
+# if we only want to generate .pem and .cert from a .crt file, we ignore everything else.
 if [ -z "$CRT_FILE" && -z "$PRIVATE_KEY" ]; then
   # generate files from .crt and exit
-  generate_files_from_crt
+  generate_file_protocol $PEM_AND_CERT_FILES
   # all done
   exit 0
 fi
 
 validate_domain "$DOMAIN"
 
-# if we want to generate a .cfg template file, ignore everything else.
+# if we only want to generate a .cfg template file, ignore everything else.
 validate_template_flag "$TEMPLATE"
 if [ "$TEMPLATE" -eq 1 ]; then
   # generate configuration template file and exit
-  generate_configuration_file_template
+  generate_file_protocol $CONFIGURATION_FILE_TEMPLATE
   # all done
   exit 0;
 fi
